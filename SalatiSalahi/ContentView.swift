a7408b0f40652382472d164363a3f11cf1d04c9d
NaifAlrashed <naialrashed@alfaisal.edu>
2019-07-08 21:00:14 +0300

    Initial Commit

---
+++ SalatiSalahi/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SalatiSalahi
+//
+//  Created by Naif Alrashed on 08/07/2019.
+//  Copyright © 2019 Naif Alrashed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
